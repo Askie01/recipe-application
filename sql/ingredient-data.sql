c5b969efaa11b378f590bc52d2bdca3477b08ea8
Askie01 <adam.paw01@gmail.com>
2025-08-08 23:55:28 +0200

    Create `ingredient-schema.sql` & `ingredient-data.sql` scripts - closes #17
    * Created SQL scripts for both table definition, and example data.
    * This commit closes #17

---
+++ sql/ingredient-data.sql
@@ -0,0 +1,35 @@
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Egg', 1.00, 1, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Donuts', 1.00, 2, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Sugar', 200.00, 3, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Saffron', 500.00, 4, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Milk', 1.00, 5, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Vanilla Extract', 5.00, 6, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Vegetable Oil', 1.00, 7, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Chicken Broth', 1.00, 8, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Blueberries', 1.00, 9, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Flour', 2.00, 10, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Olive Oil', 2.00, 11, NOW(), 'admin');
+
+INSERT INTO ingredient (name, amount, measure_unit_id, created_at, created_by)
+VALUES ('Salt', 1.00, 12, NOW(), 'admin');
