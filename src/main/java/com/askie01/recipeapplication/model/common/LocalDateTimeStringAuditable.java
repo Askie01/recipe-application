c7ec150d2a9c68b8f76235f23803132d2daac088
Askie01 <adam.paw01@gmail.com>
2025-08-08 21:42:56 +0200

    Create concrete-type common interfaces
    * Created few concrete-type interfaces that extend generic common interfaces accordingly.
    * Those interfaces are going to be used across the application in the future.

---
+++ src/main/java/com/askie01/recipeapplication/model/common/LocalDateTimeStringAuditable.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.model.common;
+
+import java.time.LocalDateTime;
+
+public interface LocalDateTimeStringAuditable
+        extends Auditable<LocalDateTime, String> {
+}
---
+++ src/main/java/com/askie01/recipeapplication/model/common/LongIdentifiable.java
@@ -0,0 +1,5 @@
+package com.askie01.recipeapplication.model.common;
+
+public interface LongIdentifiable
+        extends Identifiable<Long> {
+}
