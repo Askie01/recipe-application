5e494b521c810b91391eab38732b721696b451c4
Askie01 <adam.paw01@gmail.com>
2025-08-12 21:30:38 +0200

    Remove `@Repository` in `RecipeRepository`
    * Removed redundant annotation, as it's not doing anyway, because of spring AOP proxy.

--- src/main/java/com/askie01/recipeapplication/repository/RecipeRepository.java
+++ src/main/java/com/askie01/recipeapplication/repository/RecipeRepository.java
@@ -2,9 +2,6 @@ package com.askie01.recipeapplication.repository;
 
 import com.askie01.recipeapplication.model.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-
 }
