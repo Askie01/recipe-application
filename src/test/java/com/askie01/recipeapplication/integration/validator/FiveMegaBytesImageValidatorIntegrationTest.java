bac93b83a2b0f3393ab1556b17f8fbc190908f0a
Askie01 <adam.paw01@gmail.com>
2025-08-22 18:58:16 +0200

    Create `FiveMegaBytesImageValidatorIntegrationTest`
    * Created integration test for `FiveMegaBytesImageValidator` class.
    * Added spring context to the test, so we can simulate how this component behaves in such environment.

---
+++ src/test/java/com/askie01/recipeapplication/integration/validator/FiveMegaBytesImageValidatorIntegrationTest.java
@@ -0,0 +1,76 @@
+package com.askie01.recipeapplication.integration.validator;
+
+import com.askie01.recipeapplication.builder.TestHasImageBuilder;
+import com.askie01.recipeapplication.configuration.FiveMegaBytesImageValidatorConfiguration;
+import com.askie01.recipeapplication.model.value.HasImage;
+import com.askie01.recipeapplication.validator.ImageValidator;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        FiveMegaBytesImageValidatorConfiguration.class
+})
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@TestPropertySource(properties = "component.validator.image-type=five-mega-bytes")
+@EnabledIfSystemProperty(named = "test.type", matches = "integration")
+@DisplayName("FiveMegaBytesImageValidator integration tests")
+class FiveMegaBytesImageValidatorIntegrationTest {
+
+    private final ImageValidator validator;
+
+    @Test
+    @DisplayName("isValid method should return true when image size is above 0 MBs")
+    void isValid_whenImageSizeIsAboveZeroMegaBytes_returnsTrue() {
+        final HasImage argument = TestHasImageBuilder.builder()
+                .image(new byte[2 * 1024 * 1024])
+                .build();
+        final boolean result = validator.isValid(argument);
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("isValid method should return true when image size is exactly 5 MBs")
+    void isValid_whenImageSizeIsExactlyFiveMegaBytes_returnsTrue() {
+        final HasImage argument = TestHasImageBuilder.builder()
+                .image(new byte[5 * 1024 * 1024])
+                .build();
+        final boolean result = validator.isValid(argument);
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("isValid method should return true when image size is 0 Bytes")
+    void isValid_whenImageSizeIsZeroBytes_returnsTrue() {
+        final HasImage argument = TestHasImageBuilder.builder()
+                .image(new byte[0])
+                .build();
+        final boolean result = validator.isValid(argument);
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("isValid method should throw NullPointerException if image in HasImage is null")
+    void isValid_whenImageIsNull_throwsNullPointerException() {
+        final HasImage argument = TestHasImageBuilder.builder()
+                .image(null)
+                .build();
+        assertThrows(NullPointerException.class, () -> validator.isValid(argument));
+    }
+
+    @Test
+    @DisplayName("isValid method should throw NullPointerException if HasImage is null")
+    void isValid_whenHasImageIsNull_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> validator.isValid(null));
+    }
+}
