59edcc257e4ef348a94011952d32e15e8a225053
Askie01 <adam.paw01@gmail.com>
2025-08-24 00:25:53 +0200

    Create `ServingsMapper` interface - close #133
    * Created `ServingsMapper` interface that should perform mapping between two `HasServings` objects.
    * This commit closes #133

---
+++ src/main/java/com/askie01/recipeapplication/mapper/ServingsMapper.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.mapper;
+
+import com.askie01.recipeapplication.model.value.HasServings;
+
+public interface ServingsMapper extends Mapper<HasServings, HasServings> {
+
+}
