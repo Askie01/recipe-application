93738555b116c3025defae4cd1618b79d2859f63
Askie01 <adam.paw01@gmail.com>
2025-08-23 16:08:47 +0200

    Create `StringNameMapper` interface - close #117
    * Created a simple `StringNameMapper` interface to perform `name` field mapping from one `HasStringName` object to another.
    * This commit closes #117

---
+++ src/main/java/com/askie01/recipeapplication/mapper/StringNameMapper.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.mapper;
+
+import com.askie01.recipeapplication.model.value.HasStringName;
+
+public interface StringNameMapper extends Mapper<HasStringName, HasStringName> {
+
+}
