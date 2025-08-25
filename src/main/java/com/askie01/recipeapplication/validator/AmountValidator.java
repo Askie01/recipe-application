dcec344e5851435dee6db1485bb329c1821e4e90
Askie01 <adam.paw01@gmail.com>
2025-08-25 09:31:07 +0200

    Create `AmountValidator` interface - close #129
    * Created a simple `AmountValidator` interface to validate `HasAmount` objects.
    * This commit closes #129

---
+++ src/main/java/com/askie01/recipeapplication/validator/AmountValidator.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.validator;
+
+import com.askie01.recipeapplication.model.value.HasAmount;
+
+public interface AmountValidator extends Validator<HasAmount> {
+
+}
