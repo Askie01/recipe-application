d73b42ab834724237271caae72f366b5582738bb
Askie01 <adam.paw01@gmail.com>
2025-09-03 09:46:16 +0200

    Create `DifficultyDTOTestFactory` interface - close #211
    * Created simple `DifficultyDTOTestFactory` interface with `createDifficultyDTO` method that returns `DifficultyDTO` object.
    * This commit closes #211

---
+++ src/test/java/com/askie01/recipeapplication/factory/DifficultyDTOTestFactory.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.factory;
+
+import com.askie01.recipeapplication.dto.DifficultyDTO;
+
+public interface DifficultyDTOTestFactory {
+    DifficultyDTO createDifficultyDTO();
+}
