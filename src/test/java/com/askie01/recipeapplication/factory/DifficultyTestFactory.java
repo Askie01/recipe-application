2649a8a6ea132c41e45e1c627cb991049a3136f8
Askie01 <adam.paw01@gmail.com>
2025-09-03 03:00:28 +0200

    Create `DifficultyTestFactory` interface - close #208
    * Created `DifficultyTestFactory` interface with the `createDifficulty` method that returns a `Difficulty` object.
    * This commit closes #208

---
+++ src/test/java/com/askie01/recipeapplication/factory/DifficultyTestFactory.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.factory;
+
+import com.askie01.recipeapplication.model.entity.value.Difficulty;
+
+public interface DifficultyTestFactory {
+    Difficulty createDifficulty();
+}
