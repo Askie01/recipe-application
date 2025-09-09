0226a62d211c708aa8a10185b7742401a8358673
Askie01 <adam.paw01@gmail.com>
2025-09-09 06:44:29 +0200

    Create `AmountTestComparator` interface - close #257
    * Created a simple `AmountTestComparator` interface to perform a `compare` operation between two `HasAmount` type objects.
    * This commit closes #257

---
+++ src/test/java/com/askie01/recipeapplication/comparator/AmountTestComparator.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.comparator;
+
+import com.askie01.recipeapplication.model.value.HasAmount;
+
+public interface AmountTestComparator extends TestComparator<HasAmount, HasAmount> {
+
+}
