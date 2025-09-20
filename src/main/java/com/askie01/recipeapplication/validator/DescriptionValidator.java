46a94804eebe37576bf44608f3fafee16a4da705
Askie01 <adam.paw01@gmail.com>
2025-09-20 19:32:31 +0200

    Create `DescriptionValidator` interface - close #199
    * Created a simple `DescriptionValidator` to perform validation checks on a given `HasDescription` object.
    * This commit closes #199

---
+++ src/main/java/com/askie01/recipeapplication/validator/DescriptionValidator.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.validator;
+
+import com.askie01.recipeapplication.model.value.HasDescription;
+
+public interface DescriptionValidator extends Validator<HasDescription> {
+
+}
