9498e75ba69645114d7a1fec3415c0a07f2ba8cb
Askie01 <adam.paw01@gmail.com>
2025-09-22 11:26:14 +0200

    Fix annotation styling in `PositiveLongIdValidatorUnitTest`
    * Fixed annotation styling in `PositiveLongIdValidatorUnitTest` to make it look better.
    * This is only a cosmetic change.

--- src/test/java/com/askie01/recipeapplication/unit/validator/PositiveLongIdValidatorUnitTest.java
+++ src/test/java/com/askie01/recipeapplication/unit/validator/PositiveLongIdValidatorUnitTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("PositiveLongIdValidator unit tests")
 @EnabledIfSystemProperty(named = "test.type", matches = "unit")
+@DisplayName("PositiveLongIdValidator unit tests")
 class PositiveLongIdValidatorUnitTest {
 
     private LongIdValidator validator;
