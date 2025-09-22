46e4431487d04007c9ad66d7b73366de26712ea6
Askie01 <adam.paw01@gmail.com>
2025-09-22 10:56:05 +0200

    Update test names in `NonBlankDescriptionValidatorUnitTest`
    * Updated a few test / method names (along with @DisplayName annotations) in `NonBlankDescriptionValidatorUnitTest` for better readability.
    * This is only a cosmetic change.

--- src/test/java/com/askie01/recipeapplication/unit/validator/NonBlankDescriptionValidatorUnitTest.java
+++ src/test/java/com/askie01/recipeapplication/unit/validator/NonBlankDescriptionValidatorUnitTest.java
@@ -25,8 +25,8 @@ class NonBlankDescriptionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should return true when description is non-blank")
-    void isValid_whenDescriptionIsNonBlank_returnsTrue() {
+    @DisplayName("isValid method should return true when argument's description is non-blank")
+    void isValid_whenArgumentDescriptionIsNonBlank_returnsTrue() {
         final HasDescription argument = HasDescriptionTestBuilder.builder()
                 .description("description")
                 .build();
@@ -34,9 +34,9 @@ class NonBlankDescriptionValidatorUnitTest {
         assertTrue(result);
     }
 
-    @ParameterizedTest(name = "isValid method should return false when description is blank")
+    @ParameterizedTest(name = "isValid method should return false when argument's description is blank")
     @ValueSource(strings = {"", "   "})
-    void isValid_whenDescriptionIsBlank_returnsFalse(String description) {
+    void isValid_whenArgumentDescriptionIsBlank_returnsFalse(String description) {
         final HasDescription argument = HasDescriptionTestBuilder.builder()
                 .description(description)
                 .build();
@@ -45,8 +45,8 @@ class NonBlankDescriptionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should throw NullPointerException if description in HasDescription is null")
-    void isValid_whenDescriptionIsNull_throwsNullPointerException() {
+    @DisplayName("isValid method should throw NullPointerException if argument's description is null")
+    void isValid_whenArgumentDescriptionIsNull_throwsNullPointerException() {
         final HasDescription argument = HasDescriptionTestBuilder.builder()
                 .description(null)
                 .build();
@@ -54,7 +54,7 @@ class NonBlankDescriptionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should throw NullPointerException if HasDescription is null")
+    @DisplayName("isValid method should throw NullPointerException if argument is null")
     void isValid_whenArgumentIsNull_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> validator.isValid(null));
     }
