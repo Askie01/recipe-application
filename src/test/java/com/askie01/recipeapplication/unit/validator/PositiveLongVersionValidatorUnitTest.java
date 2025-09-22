337de191e82b28c89a4e6ce7169fae11f5060134
Askie01 <adam.paw01@gmail.com>
2025-09-22 12:47:50 +0200

    Update test names in `PositiveLongVersionValidatorUnitTest`
    * Updated a few test / method names (along with @DisplayName annotations) in `PositiveLongVersionValidatorUnitTest` for better readability.
    * This is only a cosmetic change.

--- src/test/java/com/askie01/recipeapplication/unit/validator/PositiveLongVersionValidatorUnitTest.java
+++ src/test/java/com/askie01/recipeapplication/unit/validator/PositiveLongVersionValidatorUnitTest.java
@@ -23,8 +23,8 @@ class PositiveLongVersionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should return true when source version is positive")
-    void isValid_whenSourceVersionIsPositive_returnsTrue() {
+    @DisplayName("isValid method should return true when argument's version is positive")
+    void isValid_whenArgumentVersionIsPositive_returnsTrue() {
         final HasLongVersion argument = HasLongVersionTestBuilder.builder()
                 .version(5L)
                 .build();
@@ -33,8 +33,8 @@ class PositiveLongVersionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should return false when source version is negative")
-    void isValid_whenSourceVersionIsNegative_returnsFalse() {
+    @DisplayName("isValid method should return false when argument's version is negative")
+    void isValid_whenArgumentVersionIsNegative_returnsFalse() {
         final HasLongVersion argument = HasLongVersionTestBuilder.builder()
                 .version(-5L)
                 .build();
@@ -43,8 +43,8 @@ class PositiveLongVersionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should throw NullPointerException if source version is null")
-    void isValid_whenSourceVersionIsNull_throwsNullPointerException() {
+    @DisplayName("isValid method should throw NullPointerException if argument's version is null")
+    void isValid_whenArgumentVersionIsNull_throwsNullPointerException() {
         final HasLongVersion argument = HasLongVersionTestBuilder.builder()
                 .version(null)
                 .build();
@@ -52,8 +52,8 @@ class PositiveLongVersionValidatorUnitTest {
     }
 
     @Test
-    @DisplayName("isValid method should throw NullPointerException if source is null")
-    void isValid_whenSourceIsNull_throwsNullPointerException() {
+    @DisplayName("isValid method should throw NullPointerException if argument is null")
+    void isValid_whenArgumentIsNull_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> validator.isValid(null));
     }
 }
