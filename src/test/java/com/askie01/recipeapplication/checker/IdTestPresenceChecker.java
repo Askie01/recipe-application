a34efa8c5f35846f61eaeae753ef90d6e337544a
Askie01 <adam.paw01@gmail.com>
2025-11-29 02:53:50 +0100

    Rename `IdPresenceChecker` to `IdTestPresenceChecker`
    * Fixed naming convention for the mentioned interface - it should be `IdTestPresenceChecker`

--- src/test/java/com/askie01/recipeapplication/checker/IdTestPresenceChecker.java
+++ src/test/java/com/askie01/recipeapplication/checker/IdTestPresenceChecker.java
@@ -2,6 +2,6 @@ package com.askie01.recipeapplication.checker;
 
 import com.askie01.recipeapplication.model.value.HasId;
 
-public interface IdPresenceChecker <Identifiable extends HasId<?>> {
+public interface IdTestPresenceChecker <Identifiable extends HasId<?>> {
     boolean hasId(Identifiable identifiable);
 }
