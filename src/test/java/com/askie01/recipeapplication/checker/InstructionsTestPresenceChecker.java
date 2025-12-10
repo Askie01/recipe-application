1960ee0b439d3bc3f8eb676280fb00ed3df40fc2
Adam Pawłowski <adam.paw01@gmail.com>
2025-12-10 22:55:00 +0100

    Create `InstructionsTestPresenceChecker` interface - close #349
    * Created a simple `InstructionsTestPresenceChecker` interface to perform check-like operation on a given `HasInstructions` object.
    * This commit closes #349

---
+++ src/test/java/com/askie01/recipeapplication/checker/InstructionsTestPresenceChecker.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.checker;
+
+import com.askie01.recipeapplication.model.value.HasInstructions;
+
+public interface InstructionsTestPresenceChecker {
+    boolean hasInstructions(HasInstructions hasInstructions);
+}
