cb0279a0fd4571bb4875b9050b22200baf460ea1
Adam Pawłowski <adam.paw01@gmail.com>
2025-12-12 23:59:35 +0100

    Create `CategoryTestPersistenceChecker` interface - close #360
    * Created a simple `CategoryTestPersistenceChecker` interface to check whether a given `Category` object was created or updated (in persistence context).
    * This commit closes #360

---
+++ src/test/java/com/askie01/recipeapplication/checker/CategoryTestPersistenceChecker.java
@@ -0,0 +1,7 @@
+package com.askie01.recipeapplication.checker;
+
+import com.askie01.recipeapplication.model.entity.Category;
+
+public interface CategoryTestPersistenceChecker extends TestPersistenceChecker<Category> {
+
+}
