1073e6f89f0534f91397fc440ad788c57c2dcdd0
Askie01 <adam.paw01@gmail.com>
2026-01-05 04:31:20 +0100

    Create `ValidatedLongVersionMapperTestConfigurationTemplate`
    * Created test configuration template as an entry point for creating test configurations later on.

---
+++ src/test/java/com/askie01/recipeapplication/configuration/template/ValidatedLongVersionMapperTestConfigurationTemplate.java
@@ -0,0 +1,9 @@
+package com.askie01.recipeapplication.configuration.template;
+
+import com.askie01.recipeapplication.configuration.ValidatedLongVersionMapperConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class ValidatedLongVersionMapperTestConfigurationTemplate
+        extends ValidatedLongVersionMapperConfiguration {
+}
