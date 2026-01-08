1c9e1bf91f43c6b766b04c2a0fe40e2f7628659c
Askie01 <adam.paw01@gmail.com>
2026-01-09 00:39:47 +0100

    Create `SimpleRecipeToRecipeDTOMapperTestConfigurationTemplate`
    * Created test configuration template for `SimpleRecipeToRecipeDTOMapper` to create further test configurations to register `RecipeToRecipeDTOMapper` bean in our integration tests.

---
+++ src/test/java/com/askie01/recipeapplication/configuration/template/SimpleRecipeToRecipeDTOMapperTestConfigurationTemplate.java
@@ -0,0 +1,9 @@
+package com.askie01.recipeapplication.configuration.template;
+
+import com.askie01.recipeapplication.configuration.SimpleRecipeToRecipeDTOMapperConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration
+public class SimpleRecipeToRecipeDTOMapperTestConfigurationTemplate
+        extends SimpleRecipeToRecipeDTOMapperConfiguration {
+}
