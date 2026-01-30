121de9d87856aafbd4447fc1a85f54de144f95c9
Askie01 <adam.paw01@gmail.com>
2026-01-30 14:59:10 +0100

    Update `SimpleIngredientDTOToIngredientMapperConfiguration`
    * Renamed the mentioned class to `IngredientDTOToIngredientMapperConfiguration` as we are going to store all `IngredientDTOToIngredientMapper` implementations in this configuration class for bean registration.
    * Updated property condition to register `SimpleIngredientDTOToIngredientMapper` as a default bean if no other `IngredientDTOToIngredientMapper` bean was registered.

---
+++ src/main/java/com/askie01/recipeapplication/configuration/IngredientDTOToIngredientMapperConfiguration.java
@@ -0,0 +1,29 @@
+package com.askie01.recipeapplication.configuration;
+
+import com.askie01.recipeapplication.mapper.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IngredientDTOToIngredientMapperConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "component.mapper.ingredientDTO-to-ingredient-type",
+            havingValue = "simple",
+            matchIfMissing = true
+    )
+    public IngredientDTOToIngredientMapper simpleIngredientDTOToIngredientMapper(LongIdMapper longIdMapper,
+                                                                                 StringNameMapper stringNameMapper,
+                                                                                 AmountMapper amountMapper,
+                                                                                 MeasureUnitDTOToMeasureUnitMapper measureUnitDTOToMeasureUnitMapper,
+                                                                                 LongVersionMapper longVersionMapper) {
+        return new SimpleIngredientDTOToIngredientMapper(
+                longIdMapper,
+                stringNameMapper,
+                amountMapper,
+                measureUnitDTOToMeasureUnitMapper,
+                longVersionMapper);
+    }
+}
