77a718fc03a71d7b661f024fe1538a34bbabf949
Askie01 <adam.paw01@gmail.com>
2026-01-30 15:07:16 +0100

    Create `LongIdMapperConfiguration`
    * Created class to store all `LongIdMapper` implementations for bean registration.
    * Added 2 possible beans - depending on the property.

---
+++ src/main/java/com/askie01/recipeapplication/configuration/LongIdMapperConfiguration.java
@@ -0,0 +1,32 @@
+package com.askie01.recipeapplication.configuration;
+
+import com.askie01.recipeapplication.mapper.LongIdMapper;
+import com.askie01.recipeapplication.mapper.SimpleLongIdMapper;
+import com.askie01.recipeapplication.mapper.ValidatedLongIdMapper;
+import com.askie01.recipeapplication.validator.LongIdValidator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LongIdMapperConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "component.mapper.id-type",
+            havingValue = "simple-long-id",
+            matchIfMissing = true
+    )
+    public LongIdMapper simpleLongIdMapper() {
+        return new SimpleLongIdMapper();
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "component.mapper.id-type",
+            havingValue = "validated-long-id"
+    )
+    public LongIdMapper validatedLongIdMapper(LongIdValidator longIdValidator) {
+        return new ValidatedLongIdMapper(longIdValidator);
+    }
+}
