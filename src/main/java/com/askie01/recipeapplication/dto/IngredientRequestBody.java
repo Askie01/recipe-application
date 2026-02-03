0abf273adaa63b1ae8bcf5a625afa608cb748a6c
Askie01 <adam.paw01@gmail.com>
2026-02-03 05:59:01 +0100

    Add validation to `RecipeRequestBody` & `IngredientRequestBody`
    * Added missing validation annotations for our Request bodies.

--- src/main/java/com/askie01/recipeapplication/dto/IngredientRequestBody.java
+++ src/main/java/com/askie01/recipeapplication/dto/IngredientRequestBody.java
@@ -1,5 +1,7 @@
 package com.askie01.recipeapplication.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -10,7 +12,13 @@ import lombok.*;
 @ToString
 @EqualsAndHashCode
 public class IngredientRequestBody {
+
+    @NotBlank(message = "Ingredient name cannot be blank/null")
     private String name;
+
+    @DecimalMin(value = "0.1", message = "Ingredient amount have to be at least 0.1")
     private Double amount;
+
+    @NotBlank(message = "Ingredient unit cannot be blank/null")
     private String unit;
 }
--- src/main/java/com/askie01/recipeapplication/dto/RecipeRequestBody.java
+++ src/main/java/com/askie01/recipeapplication/dto/RecipeRequestBody.java
@@ -1,6 +1,8 @@
 package com.askie01.recipeapplication.dto;
 
 import com.askie01.recipeapplication.model.entity.value.Difficulty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.util.Set;
@@ -13,13 +15,32 @@ import java.util.Set;
 @ToString
 @EqualsAndHashCode
 public class RecipeRequestBody {
+
+    @NotBlank(message = "Recipe name cannot be blank/null")
     private String name;
+
+    @Size(max = 5_242_880, message = "Recipe image cannot be larger than 5 MBs")
     private byte[] image;
+
+    @NotBlank(message = "Recipe description cannot be blank/null")
     private String description;
+
+    @NotNull(message = "Recipe difficulty has to be EASY/MEDIUM/HARD")
     private Difficulty difficulty;
+
+    @NotEmpty(message = "Recipe categories cannot be empty")
     private Set<String> categories;
+
+    @Valid
+    @NotEmpty(message = "Recipe ingredients cannot be empty")
     private Set<IngredientRequestBody> ingredients;
+
+    @DecimalMin(value = "1.0", message = "Recipe servings have to be at least 1.0")
     private Double servings;
+
+    @PositiveOrZero(message = "Recipe cooking time have to be a positive or zero number")
     private Integer cookingTime;
+
+    @NotBlank(message = "Recipe instructions cannot be blank/null")
     private String instructions;
 }
