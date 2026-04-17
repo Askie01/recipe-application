a5d8f5c62284494c9cd8313df00595cf8f634f3e
Adam Pawlowski <adam.paw01@gmail.com>
2026-04-18 00:04:03 +0200

    Create `RecipeServiceV3` interface
    * Created `RecipeServiceV3` interface to specify performed operations for incoming requests.

---
+++ src/main/java/com/askie01/recipeapplication/service/RecipeServiceV3.java
@@ -0,0 +1,28 @@
+package com.askie01.recipeapplication.service;
+
+import com.askie01.recipeapplication.dto.CustomerRecipeRequestBody;
+import com.askie01.recipeapplication.model.entity.Recipe;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface RecipeServiceV3 {
+
+    void createRecipe(String username, CustomerRecipeRequestBody requestBody);
+
+    Recipe getRecipe(Long id);
+
+    byte[] getRecipeImage(Long id);
+
+    Page<Recipe> getCustomerRecipes(String username, Pageable pageable);
+
+    Page<Recipe> searchRecipes(String query, Pageable pageable);
+
+    void updateRecipe(String username, Long id, CustomerRecipeRequestBody requestBody);
+
+    void updateImage(String username, Long id, MultipartFile image);
+
+    void restoreDefaultImage(String username, Long id);
+
+    void deleteRecipe(String username, Long id);
+}
